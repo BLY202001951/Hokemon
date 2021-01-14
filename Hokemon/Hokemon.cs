b1ceff3b381b102842be92c2d1de24693afebcd9
Louis1719 <louisblyth1719@gmail.com>
2021-01-14 12:23:38 +0000

    Add project files.

---
+++ Hokemon/Hokemon.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hokemon
+{
+    class Hokemon
+    {
+        // Members of a Class include Attributes, Properties and Methods
+
+        // Attributes
+
+        private string name;
+        private int health;
+        private int max_health;
+        private int attack;
+        private int speed;
+        private int defense;
+
+        // Properties
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Health
+        {
+            get { return health; }
+            set { health = value; }
+        }
+
+        // Methods
+
+        public Hokemon() // Constructor method runs first when instantiated
+        {
+            provide_name();
+            max_health = 100;
+            health = random_int_generator(10, max_health);
+            attack = random_int_generator(10, 100);
+            speed = random_int_generator(10, 100);
+            defense = random_int_generator(10, 100);
+        }
+
+        public void provide_name()
+        {
+            Console.WriteLine("\nEnter name for your Hokemon: ");
+            name = Console.ReadLine();
+        }
+
+        public void get_details()
+        {
+            Console.WriteLine("\n**********\nName: {0}\nHealth: {1}/{2}\nAttack value: {3}\nSpeed value: {4}\nDefense value: {5}", name, Health, max_health, attack, speed, defense);
+        }
+
+        public int random_int_generator(int min_value, int max_value) // These are parameters
+        {
+            Random rnd = new Random(); // Creating a new object, this is an instance of random class
+            int randomValue;
+            randomValue = rnd.Next(min_value, max_value); // Using the next method from the random class found in rnd object
+            return randomValue;
+        }
+    }
+}
---
+++ Hokemon/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hokemon hokeObject01 = new Hokemon();
+
+            Console.WriteLine("\nHokemon name is: {0}", hokeObject01.Name);
+
+            hokeObject01.get_details();
+
+            Hokemon hokeObject02 = new Hokemon();
+
+            Console.WriteLine("\nHokemon name is: {0}", hokeObject02.Name);
+
+            hokeObject02.get_details();
+        }
+    }
+}
